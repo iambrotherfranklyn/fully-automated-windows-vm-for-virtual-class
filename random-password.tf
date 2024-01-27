db50a88c37b7b8cf2157367ac8713eb902781ad9
iambrotherfranklyn <iambrotherfranklyn@gmail.com>
2024-01-27 23:39:17 +0000

    updated repo

--- random-password.tf
+++ random-password.tf
@@ -1,9 +1,12 @@
 resource "random_password" "password_generator" {
   for_each = toset(var.usernames)
-  length   = 11
-  lower    = true
-  #numeric          = true
-  special          = true
-  upper            = true
+  length    = 11
+  lower     = true
+  upper     = true
+  special   = true
   override_special = "!#%&"
+  min_upper = 1  
+  min_lower = 1  
+  min_special = 1 
+  
 }
