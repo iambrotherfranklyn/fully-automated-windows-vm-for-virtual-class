9eed073f1d809076994b3338d4d93f8ba156795f
iambrotherfranklyn <iambrotherfranklyn@gmail.com>
2024-01-28 18:46:15 +0000

    test commit

--- script.ps1
+++ script.ps1
@@ -1,44 +1,39 @@
 # Sign in to your Azure account
-Connect-AzAccount
+Connect-AzAccount -TenantId "6ccb7334-6f39-4d56-b82a-319cd9a5a549"
 
-#Azure Subscription I want to use
+# Azure Subscription I want to use
 $subscriptionId = "e243c19f-f5d7-4038-bdc7-a77ce40f79c3"
 
-#Resource Group my VMs are in
+# Resource Group my VMs are in
 $resourceGroup = "cyber-gees-rg"
 
-#Select the right Azure subscription
-Set-AzContext -Subscription $subscriptionId
+# Select the right Azure subscription
+Set-AzContext -SubscriptionId $subscriptionId
 
-#Get all Azure VMs which are in running state and are running Windows
-$myAzureVMs = Get-AzVM -ResourceGroupName $resourceGroup -status | Where-Object {$_.PowerState -eq "VM running" -and $_.StorageProfile.OSDisk.OSType -eq "Windows"}
-
-#Define the script to install Google Chrome
-$chromeInstallScript = {
-    # Define the URL for the Google Chrome installer
-    $chromeDownloadUrl = "https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi"
-
-    # Define the path where the installer will be downloaded
-    $downloadPath = "$env:TEMP\googlechromestandaloneenterprise64.msi"
-
-    # Download the installer
-    Invoke-WebRequest -Uri $chromeDownloadUrl -OutFile $downloadPath
-
-    # Install Google Chrome silently without user interaction
-    Start-Process 'msiexec.exe' -ArgumentList "/i $downloadPath /qn /norestart" -Wait -NoNewWindow
-
-    # Clean up - delete the installer
-    Remove-Item -Path $downloadPath -Force
+# Check if the resource group exists
+if (-Not (Get-AzResourceGroup -Name $resourceGroup -ErrorAction SilentlyContinue)) {
+    Write-Host "Resource group '$resourceGroup' could not be found." -ForegroundColor Red
+    exit
 }
 
-#Run the script against all VMs in parallel
-$myAzureVMs | ForEach-Object -Parallel {
-  $out = Invoke-AzVMRunCommand `
-  -ResourceGroupName $_.ResourceGroupName `
-  -Name $_.Name `
-  -CommandId 'RunPowerShellScript' `
-  -ScriptBlock $chromeInstallScript
-  #Formatting the Output with the VM name
-  $output = $_.Name + " " + $out.Value[0].Message
-  $output
+# Get all Azure VMs which are in running state and are running Windows
+$myAzureVMs = Get-AzVM -ResourceGroupName $resourceGroup -Status | Where-Object { $_.PowerState -eq "VM running" -and $_.StorageProfile.OsDisk.OsType -eq "Windows" }
+
+# Define the script to install Google Chrome
+$chromeInstallScript = @'
+Invoke-WebRequest -Uri "https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi" -OutFile "$env:TEMP\googlechromestandaloneenterprise64.msi"
+Start-Process 'msiexec.exe' -ArgumentList "/i `$env:TEMP\googlechromestandaloneenterprise64.msi /qn /norestart" -Wait -NoNewWindow
+Remove-Item -Path "$env:TEMP\googlechromestandaloneenterprise64.msi" -Force
+'@
+
+# Run the script against all VMs
+foreach ($vm in $myAzureVMs) {
+    $out = Invoke-AzVMRunCommand -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name -CommandId 'RunPowerShellScript' -ScriptString $chromeInstallScript
+    # Check if $out is not null and has a value
+    if ($out -and $out.Value) {
+        $output = $vm.Name + " " + $out.Value[0].Message
+    } else {
+        $output = $vm.Name + " Script execution failed or returned no output."
+    }
+    Write-Output $output
 }
